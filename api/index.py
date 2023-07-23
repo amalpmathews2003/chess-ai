b4cf569a8cde8dbdd8fb08450ebc3cf28521d786
Amal P Mathews <amalpmathews2003@gmail.com>
2023-07-23 23:21:26 +0530

    test 2

--- api/index.py
+++ api/index.py
@@ -1,9 +1,44 @@
-from ai import init_stockfish,predict_next_move,calculate_wdl
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+from stockfish import Stockfish
+
+PATH="stockfish-ubuntu-20.04-x86-64"
+
+def init_stockfish(path=PATH):
+  fish = Stockfish(path)
+  fish.update_engine_parameters({"Hash": 2048})
+
+  return fish
+
+def predict_next_move(fen):
+  fish.set_fen_position(fen)
+  next_move = fish.get_best_move()
+  wdl_stats = fish.get_wdl_stats()
+  new_fen = fish.get_fen_position()
+  return jsonify({
+      "next_move": next_move,
+      "wdl_stats": wdl_stats,
+  })
+
+def calculate_wdl(fen):
+  fish.set_fen_position(fen)
+  if fish.does_current_engine_version_have_wdl_option():
+      try:
+          wdl_stats = fish.get_wdl_stats()
+          return jsonify({"wdl_stats": wdl_stats})
+      except Exception as e:
+          print(e)
+  return  jsonify({"wdl_stats": [None,None,None]})
+
+
+fish=init_stockfish()
+
+
 
 @app.route('/', methods=['GET'])
 def home():
