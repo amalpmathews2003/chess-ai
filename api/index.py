3720a1fc80d623821d7d7e9cd4f5438cba4dc069
Amal P Mathews <amalpmathews2003@gmail.com>
2023-07-23 23:15:29 +0530

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from ai import init_stockfish,predict_next_move,calculate_wdl
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return 'Hello, World!'
+  return "home"
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('/next', methods=['POST'])
+def next_move():
+    json = request.json
+    fen = json['fen'] 
+    return predict_next_move(fen)
+
+@app.route('/wdl', methods=['POST'])
+def wdl_stats():
+    json = request.json
+    fen = json['fen']
+    return calculate_wdl(fen)
